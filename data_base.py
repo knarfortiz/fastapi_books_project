26b8c88ad11044f49414001a5f5882db6c51a256
knarfortiz <o.g.knarf@hotmail.com>
2025-02-11 09:24:26 -0500

    base get all books

---
+++ data_base.py
@@ -0,0 +1,22 @@
+from models import Book
+
+BOOKS: list[Book] = [
+    {"title": "Title One", "author": "Author One", "category": "science"},
+    {"title": "Title Two", "author": "Author Two", "category": "science"},
+    {"title": "Title Three", "author": "Author Three", "category": "history"},
+    {"title": "Title Four", "author": "Author Four", "category": "math"},
+    {"title": "Title Five", "author": "Author Five", "category": "math"},
+    {"title": "Title Six", "author": "Author Two", "category": "math"},
+    {"title": "Title Seven", "author": "Author Two", "category": "math"},
+    {"title": "Title Eight", "author": "Author Two", "category": "math"},
+    {"title": "Title Nine", "author": "Author Two", "category": "math"},
+    {"title": "Title Ten", "author": "Author Two", "category": "math"},
+    {"title": "Title Eleven", "author": "Author Two", "category": "math"},
+    {"title": "Title Twelve", "author": "Author Two", "category": "math"},
+    {"title": "Title Thirteen", "author": "Author Two", "category": "math"},
+    {"title": "Title Fourteen", "author": "Author Two", "category": "math"},
+    {"title": "Title Fifteen", "author": "Author Two", "category": "math"},
+    {"title": "Title Sixteen", "author": "Author Two", "category": "math"},
+    {"title": "Title Seventeen", "author": "Author Two", "category": "math"},
+    {"title": "Title Eighteen", "author": "Author Two", "category": "math"},
+]
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from typing import Union
-
 from fastapi import FastAPI
+
+from data_base import BOOKS
+from models import Book
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/books")
+async def get_books() -> list[Book]:
+    return BOOKS
