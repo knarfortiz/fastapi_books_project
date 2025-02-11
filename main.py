fd830d28a30905ce3da655572c392cb5a907d493
knarfortiz <o.g.knarf@hotmail.com>
2025-02-11 10:38:39 -0500

    find book by title

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ app = FastAPI()
 @app.get("/books")
 async def get_books() -> list[Book]:
     return BOOKS
+
+
+@app.get("/books/{book_title}")
+async def get_book_by_name(book_title: str) -> Book | str:
+    return next(
+        (book for book in BOOKS if book["title"].lower() == book_title.lower()),
+        "Not found",
+    )
